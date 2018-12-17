684e414beb1004437835e11502171a7b38360570
mawenlong <15061886113@163.com>
2018-12-17 21:48:53 +0800

    商品服务

---
+++ product/src/main/java/com/mwl/product/controller/ProductController.java
@@ -0,0 +1,17 @@
+package com.mwl.product.controller;
+
+/**
+ * @author mawenlong
+ * @date 2018/12/17
+ */
+public class ProductController {
+    /**
+     * 1.
+     * 2.
+     * 3.
+     * 4.
+     */
+    public void list(){
+
+    }
+}
