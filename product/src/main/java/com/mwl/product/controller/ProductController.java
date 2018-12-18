b014da21c3a8cd51db40420d191e7a915b00b098
mawenlong <15061886113@163.com>
2018-12-18 10:17:15 +0800

    查询商品列表

--- product/src/main/java/com/mwl/product/controller/ProductController.java
+++ product/src/main/java/com/mwl/product/controller/ProductController.java
@@ -11,6 +11,8 @@ import com.mwl.product.utils.ResultVOUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -66,5 +68,17 @@ public class ProductController {
         }
 
         return ResultVOUtil.success(productVOList);
+    }
+
+    /**
+     * 获取商品列表(给订单服务用的)
+     *
+     * @param productIdList
+     *
+     * @return
+     */
+    @PostMapping("/listForOrder")
+    public List<ProductVO> listForOrder(@RequestBody List<String> productIdList) {
+        return null;
     }
 }
