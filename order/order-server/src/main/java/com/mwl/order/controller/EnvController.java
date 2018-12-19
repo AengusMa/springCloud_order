6d926cc005ababe1f121d63d6490522d388e4ec6
mawenlong <15061886113@163.com>
2018-12-19 22:29:26 +0800

    配置中心

---
+++ order/order-server/src/main/java/com/mwl/order/controller/EnvController.java
@@ -0,0 +1,23 @@
+package com.mwl.order.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author mawenlong
+ * @date 2018/12/19
+ */
+@RestController
+@RequestMapping("env")
+public class EnvController {
+
+    @Value("${env}")
+    public String env;
+
+    @GetMapping("/print")
+    public String print(){
+        return env;
+    }
+}
